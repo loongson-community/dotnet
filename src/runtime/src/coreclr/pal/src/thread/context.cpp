916b4606b76bcd1d6f6c1e018732ddd46c3179bf
Xu Liangyu <xuliangyu@loongson.cn>
2025-08-02 20:38:34 +0800

    Handle LASX/LSX/FP context depending on ISA availability. (#118007)
    * [LoongArch64] Fix SDK running on 2K series SoC.
    
    ---------
    
    [dotnet-unofficial-build]: cherry-picked from commit dotnet/runtime@6dc15b56585886ca5543d1b907a37c1f4efe5410
    
    Co-authored-by: driver1998 <driver1998.ms@outlook.com>

--- src/runtime/src/coreclr/pal/src/thread/context.cpp
+++ src/runtime/src/coreclr/pal/src/thread/context.cpp
@@ -1101,6 +1101,7 @@ void CONTEXTFromNativeContext(const native_context_t *native, LPCONTEXT lpContex
             lpContext->Fcsr = fpr->fcsr;
             lpContext->Fcc  = fpr->fcc;
             memcpy(lpContext->F, fpr->regs, sizeof(fpr->regs));
+            lpContext->ContextFlags |= CONTEXT_LSX;
         }
         else if (LASX_CTX_MAGIC == info->magic)
         {
@@ -1108,6 +1109,7 @@ void CONTEXTFromNativeContext(const native_context_t *native, LPCONTEXT lpContex
             lpContext->Fcsr = fpr->fcsr;
             lpContext->Fcc  = fpr->fcc;
             memcpy(lpContext->F, fpr->regs, sizeof(fpr->regs));
+            lpContext->ContextFlags |= CONTEXT_LASX;
         }
         else
         {
